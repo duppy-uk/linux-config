cbd3338e0d83f4b602f83ee3b3b41b36f034c869
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-01-21 08:29:37 +0000

    Add malcontent-gui and btop to installation script

--- ubuntu-software-install.sh
+++ ubuntu-software-install.sh
@@ -2,10 +2,11 @@ sudo apt update
 
 # Install software repositories & store
 
-sudo apt install -y flatpak
+sudo apt install -y flatpak malcontent-gui
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo apt install -y gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
 
 # Install small utilities
-sudo apt install -y joe nala gnome-shell-extension-manager xfce4-terminal
+sudo apt install -y joe nala gnome-shell-extension-manager xfce4-terminal btop
 
+sudo apt install -y build-essential
