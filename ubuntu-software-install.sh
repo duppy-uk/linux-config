8ad29c0fd4384ebea1aa16037be9f700a6ad963c
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-01-27 17:03:42 +0000

    Refactor ubuntu-software-install.sh for improved execution
    Removed 'sudo' from package installation commands and added a shebang. Updated the script to include apt upgrade and clone the nerd-fonts repository directly.

--- ubuntu-software-install.sh
+++ ubuntu-software-install.sh
@@ -1,18 +1,25 @@
-sudo add-apt-repository universe
-sudo add-apt-repository multiverse
-sudo apt update
+#!/bin/bash
+
+# Add additional repositories
+add-apt-repository universe
+add-apt-repository multiverse
+apt update && apt upgrade
 
 # Install software repositories & store
 
-sudo apt install -y flatpak malcontent-gui plasma-discover-backend-flatpak
+apt install -y flatpak malcontent-gui plasma-discover-backend-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo apt install -y gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
+apt install -y gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
 
 # Install small utilities
-sudo apt install -y joe nala xfce4-terminal btop onedriver
+apt install -y joe nala xfce4-terminal btop onedriver
 
-sudo apt install -y build-essential kubuntu-restricted-extras
+apt install -y build-essential kubuntu-restricted-extras
 
 # Install fonts
-sudo apt install -y fonts-cascadia-code fonts-cherrybomb fonts-chomsky fonts-firacode fonts-gnutypewriter fonts-humor-sans fonts-kode-mono fonts-montserrat fonts-opendyslexic fonts-roboto
-git clone https://github.com/ryanoasis/nerd-fonts/blob/master/readme.md
+apt install -y fonts-cascadia-code fonts-cherrybomb fonts-chomsky fonts-firacode fonts-gnutypewriter fonts-humor-sans fonts-kode-mono fonts-montserrat fonts-opendyslexic fonts-roboto
+git clone https://github.com/ryanoasis/nerd-fonts
+
+apt install -y kdenlive krita
+
+sudo snap install obsidian --classic
