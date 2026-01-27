9c372bd9720fe1fb58f8645299b3cc8899d73ef8
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-01-27 15:50:40 +0000

    Install onedriver along with other utilities
    Added onedriver to the list of small utilities to install.

--- ubuntu-software-install.sh
+++ ubuntu-software-install.sh
@@ -9,6 +9,6 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 sudo apt install -y gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
 
 # Install small utilities
-sudo apt install -y joe nala xfce4-terminal btop
-
+sudo apt install -y joe nala xfce4-terminal btop onedriver
+sudo apt install -y fonts-cascadia-code fonts-cherrybomb fonts-chomsky fonts-firacode fonts-gnutypewriter fonts-humor-sans fonts-kode-mono fonts-montserrat fonts-opendyslexic fonts-roboto
 sudo apt install -y build-essential kubuntu-restricted-extras
