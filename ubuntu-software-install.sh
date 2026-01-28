6f9d03db030317cef0b697953d76dceb6e9cf650
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-01-28 13:31:02 +0000

    Add VS Code installation to script
    Added installation steps for Visual Studio Code including repository setup and GPG key import.

--- ubuntu-software-install.sh
+++ ubuntu-software-install.sh
@@ -11,6 +11,16 @@ apt install -y flatpak malcontent-gui plasma-discover-backend-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 apt install -y gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
 
+# Install VS Code
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor \
+  | install -D -o root -g root -m 644 /dev/stdin /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] \
+  https://packages.microsoft.com/repos/code stable main" \
+  | tee /etc/apt/sources.list.d/vscode.list
+apt update
+apt install code
+
 # Install small utilities
 apt install -y joe nala xfce4-terminal btop onedriver filezilla 
 
