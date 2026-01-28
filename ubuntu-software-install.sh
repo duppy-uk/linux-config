313cba42ea7a9f44417feaae30d8f261bbebad23
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-01-28 12:13:08 +0000

    Update software installation script with new packages
    Added filezilla, akregator, and various flatpak applications including Audacity, Parlatype, and Jitsi Meet. Commented out musescore installation commands.

--- ubuntu-software-install.sh
+++ ubuntu-software-install.sh
@@ -12,7 +12,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 apt install -y gnome-software gnome-software-plugin-flatpak gnome-software-plugin-snap
 
 # Install small utilities
-apt install -y joe nala xfce4-terminal btop onedriver
+apt install -y joe nala xfce4-terminal btop onedriver filezilla 
 
 apt install -y build-essential kubuntu-restricted-extras
 
@@ -20,7 +20,7 @@ apt install -y build-essential kubuntu-restricted-extras
 apt install -y fonts-cascadia-code fonts-cherrybomb fonts-chomsky fonts-firacode fonts-gnutypewriter fonts-humor-sans fonts-kode-mono fonts-montserrat fonts-opendyslexic fonts-roboto
 git clone https://github.com/ryanoasis/nerd-fonts
 
-apt install -y kdenlive krita
+apt install -y kdenlive krita akregator
 
 # Install snaps
 
@@ -36,11 +36,11 @@ sudo snap install deepseek-desktop
 sudo snap install discord
 snap connect discord:system-observe
 
-sudo snap install musescore
-sudo snap connect musescore:cups-control
-sudo snap connect musescore:network-manager
-sudo snap connect musescore:alsa
-sudo snap connect musescore:removable-media
+# sudo snap install musescore
+# sudo snap connect musescore:cups-control
+# sudo snap connect musescore:network-manager
+# sudo snap connect musescore:alsa
+# sudo snap connect musescore:removable-media
 
 sudo snap install pocket-casts
 sudo snap install apple-music-for-linux
@@ -53,8 +53,28 @@ sudo snap install steam
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub io.github.giantpinkrobots.flatsweep
 
+flatpak install flathub io.github.smallbasic.SmallBASIC
+
+flatpak install flathub com.vivaldi.Vivaldi
+flatpak install flathub com.microsoft.Edge
+flatpak install flathub org.pvermeer.WebAppHub
+flatpak install flathub io.github.zaedus.spider
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub org.signal.Signal
+
 flatpak install flathub com.vivaldi.Vivaldi
 flatpak install flathub md.obsidian.Obsidian
-
+flatpak install flathub org.zotero.Zotero
+flatpak install flathub com.jgraph.drawio.desktop
+flatpak install flathub org.onlyoffice.desktopeditors
+flatpak install flathub org.gramps_project.Gramps
+flatpak install flathub org.kde.tellico
 flatpak install flathub org.musescore.MuseScore
-
+flatpak install flathub org.audacityteam.Audacity
+# flatpak install flathub org.kde.krita
+flatpak install flathub in.co.nandakumar.vara
+flatpak install flathub xyz.parlatype.Parlatype
+flatpak install flathub com.cinecred.cinecred
+flatpak install flathub org.jitsi.jitsi-meet
+flatpak install flathub com.github.louis77.tuner
+flatpak install flathub io.github.Archeb.opentrace
