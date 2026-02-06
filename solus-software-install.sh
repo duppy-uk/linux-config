19f41798e814129a3871d7903a18cf2ab4f20106
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-02-06 09:49:49 +0000

    Convert installation list to a script format
    Refactor software installation commands into a script format.

--- solus-software-install.sh
+++ solus-software-install.sh
@@ -1,32 +1,36 @@
-sudo eopkg install adwaita-colors-icon-theme \
-adwaita-fonts \
-adwaita-icon-theme \
-akregator \
-android-tools \
-anoise \
-anoise-gui \
-anoise-media \
-anoise-media-community-extension1 \
-anoise-media-community-extension2 \
-anoise-media-community-extension3 \
-anoise-media-community-extension4 \
-anoise-media-community-extension5 \
-arc-icon-theme \
-ardour \
-arp-scan \
-audacious \
-audacious-plugins \
-audacity \
-bcc \
-binutils \
-bitwarden-cli \
-bitwarden-desktop \
-bluefish \
-brave \
-btop \
-budgie-backgrounds \
-cabextract \
+#!/bin/bash
 
+adwaita-colors-icon-theme
+adwaita-fonts
+adwaita-icon-theme
+akregator
+android-tools
+anoise
+anoise-gui
+anoise-media
+anoise-media-community-extension1
+anoise-media-community-extension2
+anoise-media-community-extension3
+anoise-media-community-extension4
+anoise-media-community-extension5
+arc-icon-theme
+ardour
+arp-scan
+audacious
+audacious-plugins
+audacity
+bcc
+binutils
+bitwarden-cli
+bitwarden-desktop
+bluefish
+brave
+btop
+budgie-backgrounds
+cabextract
+calibre
+
+sudo eopkg install 
 
 ## Git repos
 #https://github.com/solus-project/artwork
