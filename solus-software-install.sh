692ef350441a7e94ae9cc2865c187c93b20e40c2
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-02-06 10:17:40 +0000

    Refactor solus-software-install.sh to use eopkg commands
    Added installation commands for various packages and updated the script to include system upgrade commands.

--- solus-software-install.sh
+++ solus-software-install.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 
-adwaita-colors-icon-theme
-adwaita-fonts
-adwaita-icon-theme
-akregator
-android-tools
-anoise
-anoise-gui
-anoise-media
-anoise-media-community-extension1
-anoise-media-community-extension2
-anoise-media-community-extension3
-anoise-media-community-extension4
-anoise-media-community-extension5
-arc-icon-theme
-ardour
-arp-scan
-audacious
-audacious-plugins
-audacity
-bcc
-binutils
-bitwarden-cli
-bitwarden-desktop
-bluefish
-brave
-btop
-budgie-backgrounds
-cabextract
-calibre
+sudo eopkg -y upgrade
+
+sudo eopkg -y install adwaita-colors-icon-theme
+sudo eopkg -y install adwaita-fonts
+sudo eopkg -y install adwaita-icon-theme
+sudo eopkg -y install akregator
+sudo eopkg -y install android-tools
+sudo eopkg -y install anoise
+sudo eopkg -y install anoise-gui
+sudo eopkg -y install anoise-media
+sudo eopkg -y install anoise-media-community-extension1
+sudo eopkg -y install anoise-media-community-extension2
+sudo eopkg -y install anoise-media-community-extension3
+sudo eopkg -y install anoise-media-community-extension4
+sudo eopkg -y install anoise-media-community-extension5
+sudo eopkg -y install arc-icon-theme
+sudo eopkg -y install ardour
+sudo eopkg -y install arp-scan
+sudo eopkg -y install audacious
+sudo eopkg -y install audacious-plugins
+sudo eopkg -y install audacity
+sudo eopkg -y install bcc
+sudo eopkg -y install binutils
+sudo eopkg -y install bitwarden-cli
+sudo eopkg -y install bitwarden-desktop
+sudo eopkg -y install bluefish
+sudo eopkg -y install brave
+sudo eopkg -y install btop
+sudo eopkg -y install budgie-backgrounds
+sudo eopkg -y install cabextract
+sudo eopkg -y install calibre
+
+sudo eopkg -y install vscode
+sudo eopkg upgrade -y
 
 sudo eopkg install 
+
+echo "== SETTINGS =="
+# Add global git values
+git config --global user.name "Darren Upton"
+git config --global user.email "dbupton@outlook.com"
+
+sudo eopkg clean packages -y
 
 ## Git repos
 #https://github.com/solus-project/artwork
