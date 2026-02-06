95489a74de710977ad5628575e74df7bdc603129
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-02-06 09:32:14 +0000

    Create solus-software-install.sh for software installation
    Add script to install various software packages on Solus.

---
+++ solus-software-install.sh
@@ -0,0 +1,32 @@
+sudo eopkg install adwaita-colors-icon-theme \
+adwaita-fonts \
+adwaita-icon-theme \
+akregator \
+android-tools \
+anoise \
+anoise-gui \
+anoise-media \
+anoise-media-community-extension1 \
+anoise-media-community-extension2 \
+anoise-media-community-extension3 \
+anoise-media-community-extension4 \
+anoise-media-community-extension5 \
+arc-icon-theme \
+ardour \
+arp-scan \
+audacious \
+audacious-plugins \
+audacity \
+bcc \
+binutils \
+bitwarden-cli \
+bitwarden-desktop \
+bluefish \
+brave \
+btop \
+budgie-backgrounds \
+cabextract \
+
+
+## Git repos
+#https://github.com/solus-project/artwork
