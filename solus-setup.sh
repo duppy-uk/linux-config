f37e30c89abad5de8fd3bb113bf392779ae78579
Darren <75955117+duppy-uk@users.noreply.github.com>
2026-02-07 12:54:46 +0000

    Remove 'sudo' from eopkg commands in setup script

--- solus-setup.sh
+++ solus-setup.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
 # Disable tmpfs going to RAM
-sudo systemctl mask temp.mount
+systemctl mask temp.mount
 
 #Get the system up-to-date before any new installations
-sudo eopkg -y upgrade
+eopkg -y upgrade
 
-sudo eopkg -y install adwaita-colors-icon-theme
-sudo eopkg -y install adwaita-fonts
-sudo eopkg -y install adwaita-icon-theme
-sudo eopkg -y install akregator
-sudo eopkg -y install android-tools
-sudo eopkg -y install anoise
-sudo eopkg -y install anoise-gui
-sudo eopkg -y install anoise-media
-sudo eopkg -y install anoise-media-community-extension1
-sudo eopkg -y install anoise-media-community-extension2
-sudo eopkg -y install anoise-media-community-extension3
-sudo eopkg -y install anoise-media-community-extension4
-sudo eopkg -y install anoise-media-community-extension5
-sudo eopkg -y install arc-icon-theme
-sudo eopkg -y install ardour
-sudo eopkg -y install arp-scan
-sudo eopkg -y install audacious
-sudo eopkg -y install audacious-plugins
-sudo eopkg -y install audacity
-sudo eopkg -y install bcc
-sudo eopkg -y install binutils
-sudo eopkg -y install bitwarden-cli
-sudo eopkg -y install bitwarden-desktop
-sudo eopkg -y install bluefish
-sudo eopkg -y install brave
-sudo eopkg -y install btop
-sudo eopkg -y install budgie-backgrounds
-sudo eopkg -y install cabextract
-sudo eopkg -y install calibre
+eopkg -y install adwaita-colors-icon-theme
+eopkg -y install adwaita-fonts
+eopkg -y install adwaita-icon-theme
+eopkg -y install akregator
+eopkg -y install android-tools
+eopkg -y install anoise
+eopkg -y install anoise-gui
+eopkg -y install anoise-media
+eopkg -y install anoise-media-community-extension1
+eopkg -y install anoise-media-community-extension2
+eopkg -y install anoise-media-community-extension3
+eopkg -y install anoise-media-community-extension4
+eopkg -y install anoise-media-community-extension5
+eopkg -y install arc-icon-theme
+eopkg -y install ardour
+eopkg -y install arp-scan
+eopkg -y install audacious
+eopkg -y install audacious-plugins
+eopkg -y install audacity
+eopkg -y install bcc
+eopkg -y install binutils
+eopkg -y install bitwarden-cli
+eopkg -y install bitwarden-desktop
+eopkg -y install bluefish
+eopkg -y install brave
+eopkg -y install btop
+eopkg -y install budgie-backgrounds
+eopkg -y install cabextract
+eopkg -y install calibre
 
-sudo eopkg -y install vscode
-sudo eopkg upgrade -y
+eopkg -y install solseek
+eopkg -y install vscode
 
-sudo eopkg install 
 
 echo "== SETTINGS =="
 
@@ -48,7 +47,8 @@ echo "== SETTINGS =="
 #git config --global user.name "Darren Upton"
 #git config --global user.email "dbupton@outlook.com"
 
-sudo eopkg clean packages -y
+echo "== CLEAN UP =="
+eopkg clean packages -y
 
 ## Git repos
 #https://github.com/solus-project/artwork
